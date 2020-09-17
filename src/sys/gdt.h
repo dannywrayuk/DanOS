9e51d93b05d60a600c38f3da31ce5a90b102f8e0
dannywrayuk <dannywray97@gmail.com>
2020-09-17 19:23:50 +0100

    gdt structures

---
+++ src/sys/gdt.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <stdint.h>
+#include <misc/packed.h>
+
+namespace sys
+{
+
+    struct gdt_entry
+    {
+        uint16_t limit_low;  // The lower 16 bits of the limit.
+        uint16_t base_low;   // The lower 16 bits of the base.
+        uint8_t base_middle; // The next 8 bits of the base.
+        uint8_t access;      // Access flags, determine what ring this segment can be used in.
+        uint8_t granularity;
+        uint8_t base_high; // The last 8 bits of the base.
+    } __attribute__((packed));
+
+    struct tss_entry
+    {
+        uint16_t length;
+        uint16_t base_low16;
+        uint8_t base_mid8;
+        uint8_t flags1;
+        uint8_t flags2;
+        uint8_t base_high8;
+        uint32_t base_upper32;
+        uint32_t reserved;
+    } __attribute__((packed));
+
+    struct gdt
+    {
+        struct gdt_entry entries[5];
+        struct tss_entry tss;
+    } __attribute__((packed));
+
+    struct gdt_pointer
+    {
+        uint16_t size;
+        uint64_t address;
+    } __attribute__((packed));
+
+    void gdtSetGate(uint32_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran);
+
+    void initGDT();
+
+    uint64_t getGDTptr();
+
+} // namespace sys
